eff262cba22ce0e2c9fa3bf881d15477d50d804c
LeuAlmeida <leo@webid.net.br>
2020-02-10 18:55:45 -0300

    feat: introducing float menu into index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
+import FloatMenu from 'components/flags';
 
 export default () => (
   <Layout>
     <SEO />
+    <FloatMenu />
     <Intro />
     <Projects />
     <Skills />
