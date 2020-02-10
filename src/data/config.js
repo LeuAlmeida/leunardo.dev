3b1bc4dec609626c5c6a64d5e6e89bb5c6c14ba5
LeuAlmeida <leo@webid.net.br>
2020-02-10 18:47:20 -0300

    chore: update my name

--- src/data/config.js
+++ src/data/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  defaultTitle: 'John Doe',
+  defaultTitle: 'Léu Almeida',
   logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
-  author: 'John Doe',
+  author: 'Léu Almeida',
   url: 'https://portfolio.smakosh.com',
-  legalName: 'John Doe',
+  legalName: 'Léu Almeida',
   defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
   socialLinks: {
     twitter: 'http://www.twitter.com/smakosh',
