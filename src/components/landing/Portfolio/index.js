3e6c4c462b79cc7edcc6585a66412fcc87c00b1a
Léu Almeida <leo@webid.net.br>
2020-02-11 00:04:04 -0300

    feat: initial structure of portfolio index file

---
+++ src/components/landing/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Grid } from './styles';
+
+export const Portfolio = () => (
+  <Container>
+    <Grid>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </Grid>
+  </Container>
+);
