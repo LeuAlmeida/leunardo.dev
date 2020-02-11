f650a181f395b7efe4481e50c275917f24a76ed0
LeuAlmeida <leo@webid.net.br>
2020-02-11 12:25:02 -0300

    feat: incrementing more content

--- src/components/landing/Portfolio/index.js
+++ src/components/landing/Portfolio/index.js
@@ -11,8 +11,30 @@ export const Portfolio = () => (
         <ImagePortfolio
           boldName="BTM"
           name="Performance"
-          github="https://github.com/LeuAlmeida"
-          image={images.aircnc}
+          behance="https://www.behance.net/gallery/78028243/Projeto-de-Website-BTM-Performance"
+          url="https://btmperformance.com.br/"
+          image={images.btm}
+        />
+        <ImagePortfolio
+          boldName="APP"
+          name="October"
+          behance="https://www.behance.net/gallery/87664595/React-Native-open-source-App-October"
+          github="https://github.com/LeuAlmeida/app.october"
+          image={images.appoctober}
+        />
+        <ImagePortfolio
+          boldName="T3"
+          name="Driver"
+          behance="https://www.behance.net/gallery/73684575/Criacao-de-Website-T3-Driver"
+          url="https://t3driver.com.br/"
+          image={images.t3Driver}
+        />
+        <ImagePortfolio
+          boldName="E-mail"
+          name="Marketing"
+          github="https://github.com/LeuAlmeida/E-mail-Marketing-Portfolio"
+          behance="https://github.com/LeuAlmeida/E-mail-Marketing-Portfolio"
+          image={images.emkt}
         />
       </Grid>
     </Container>
