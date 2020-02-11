dc688ebd3f79344f4e7352a120ced3f41a9423f0
LeuAlmeida <leo@webid.net.br>
2020-02-11 12:11:42 -0300

    style: improving portfolio grid

--- src/components/landing/Portfolio/styles.js
+++ src/components/landing/Portfolio/styles.js
@@ -10,8 +10,15 @@ export const Container = styled.div`
 
 export const Grid = styled.div`
   display: grid;
-  width: 80%;
+  width: 100%;
   align-items: center;
   grid-template-columns: repeat(3, 1fr);
   gap: 5px;
+`;
+
+export const PortfolioWrapper = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
