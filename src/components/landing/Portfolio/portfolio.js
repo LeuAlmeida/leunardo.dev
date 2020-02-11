fb684b279ea5cfea494a8cd43ae439f3a99c7abf
LeuAlmeida <leo@webid.net.br>
2020-02-11 15:11:14 -0300

    add: create portfolio js file

---
+++ src/components/landing/Portfolio/portfolio.js
@@ -0,0 +1,52 @@
+import * as images from './images';
+
+export const portfolio = [
+  {
+    boldName: 'BTM',
+    name: 'Performance',
+    behance: 'https://www.behance.net/gallery/78028243/Projeto-de-Website-BTM-Performance',
+    url: 'https://btmperformance.com.br/',
+    image: images.btm,
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    boldName: 'APP',
+    name: 'October',
+    behance: 'https://www.behance.net/gallery/87664595/React-Native-open-source-App-October',
+    github: 'https://github.com/LeuAlmeida/app.october',
+    image: images.appoctober,
+    category: ['all', 'mobile', 'ux-ui'],
+  },
+  {
+    boldName: 'T3',
+    name: 'Driver',
+    behance: 'https://www.behance.net/gallery/73684575/Criacao-de-Website-T3-Driver',
+    url: 'https://t3driver.com.br/',
+    image: images.t3Driver,
+    category: ['all', 'frontend'],
+  },
+  {
+    boldName: 'TOTVS',
+    name: 'Project',
+    github: 'https://github.com/LeuAlmeida/TOTVS-Front-End---Educa--o-Metodista',
+    behance: 'https://www.behance.net/gallery/83707895/ERP-Project-Methodist-Education',
+    image: images.totvs,
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    boldName: 'E-mail',
+    name: 'Marketing',
+    github: 'https://github.com/LeuAlmeida/E-mail-Marketing-Portfolio',
+    behance: 'https://www.behance.net/gallery/70186937/Criacao-e-Envio-de-E-mail-Mkt-Educacao-Metodista',
+    image: images.emkt,
+    category: ['all', 'others'],
+  },
+  {
+    boldName: 'Guiga',
+    name: 'Peixoto',
+    video: 'https://www.facebook.com/watch/?v=2831370423754902',
+    behance: 'https://www.behance.net/gallery/81497053/Producao-Audiovisual-Dep-Federal-Guiga-Peixoto',
+    image: images.guigaPeixoto,
+    category: ['all', 'others'],
+  },
+];
