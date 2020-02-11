553cad0232dfde25c54b931c02ca797586fc7402
LeuAlmeida <leo@webid.net.br>
2020-02-11 17:45:40 -0300

    feat: incrementing  section

--- src/components/landing/Skills/index.js
+++ src/components/landing/Skills/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Container } from 'components/common';
 import dev from 'assets/illustrations/skills.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
 
 export const Skills = () => (
-  <Wrapper id="portfolio">
+  <Wrapper id="myself">
     <SkillsWrapper as={Container}>
       <Thumbnail>
         <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
       </Thumbnail>
       <Details>
-        <h1>Portfolio</h1>
+        <h1>Who I am</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+          Hello! My name is Leonardo Almeida, but you can call me Léu :)
+          <br />
+          I’m 20 years old and I’m a tech enthusiast with a focus on front-end. Alongside this I also seek to learn
+          about back-end so I can become a full stack developer. I also have experience on audiovisual production, 3D
+          modeling and Graphic Design for occasional needs.
+          <br />
+          I’ve been involved with this market for a decade, when I was only 10 years old and started studying Basic
+          HTML. Since then my crave for learning only got bigger.
+          <br />
+          My projects are focused on the user experience and it’s usability as a whole, aiming for a good relationship
+          between user and platform - whether it’s a system or a full website.
+          <br />
+        </p>
+        <p>
+          From my perspective, proactivity and optimism should walk side by side in a way that great results are
+          achieved on a relaxed workspace.
+        </p>
+        <p>
+          Now that you know a little about me, <AnchorLink href="#contact">let’s grab a coffee?</AnchorLink>
         </p>
       </Details>
     </SkillsWrapper>
