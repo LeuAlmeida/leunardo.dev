4ce241ac319db748c81ed6a5c13abdf76eb80f24
LeuAlmeida <leo@webid.net.br>
2020-02-11 11:35:21 -0300

    feat: introducing initial content of Portfolio section

--- src/components/landing/Portfolio/index.js
+++ src/components/landing/Portfolio/index.js
@@ -4,12 +4,18 @@ import { Container, Grid } from './styles';
 
 import ImagePortfolio from '../../common/ImagePortfolio';
 
+import aircnc from '../../../assets/portfolio/aircnc.jpg';
+
 export const Portfolio = () => (
   <Container>
     <Grid>
-      <ImagePortfolio />
-      <ImagePortfolio />
-      <ImagePortfolio />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
+      <ImagePortfolio name="Air" boldName="CnC" github="https://github.com/LeuAlmeida" image={aircnc} />
     </Grid>
   </Container>
 );
