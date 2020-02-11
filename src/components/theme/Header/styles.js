7219f5782bd47116d9f85004e7481dd10d34997b
LeuAlmeida <leo@webid.net.br>
2020-02-11 17:52:11 -0300

    fix: update navigation fixed on top

--- src/components/theme/Header/styles.js
+++ src/components/theme/Header/styles.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background: transparent;
   width: 100%;
+  background: rgba(250, 250, 250, 0.5);
+  height: 65px;
+  position: fixed;
+  z-index: 5555;
 `;
 
 export const Overlay = styled.div`
   position: fixed;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(0, 0, 0, 0.8);
   width: 100%;
   height: 100%;
   display: none;
