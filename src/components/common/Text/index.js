0a4284d9a24b2bcfd651ef9c6f667069c9de6cfd
LeuAlmeida <leo@webid.net.br>
2020-02-11 17:46:45 -0300

    feat: create common text styled components

---
+++ src/components/common/Text/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 10%;
+  width: 200px;
+`;
+
+export const H2 = styled.h2`
+  font-size: 32px;
+  color: #7178fc;
+  margin-bottom: 0 !important;
+
+  &:before {
+    content: '';
+    margin-bottom: 4px;
+    display: block;
+    width: 50px;
+    height: 5px;
+    border-radius: 10px;
+    background: #7178fc;
+  }
+`;
+
+export const SubTitle = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+  color: #666;
+`;
