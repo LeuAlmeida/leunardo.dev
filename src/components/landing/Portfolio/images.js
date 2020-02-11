e3bfd23a62d4e767fce7989360455a4fb6719f1a
LeuAlmeida <leo@webid.net.br>
2020-02-11 12:18:52 -0300

    feat: introducing portfolio images

--- src/components/landing/Portfolio/images.js
+++ src/components/landing/Portfolio/images.js
@@ -1,5 +1,21 @@
 const aircnc = require('../../../assets/portfolio/aircnc.jpg');
+const appoctober = require('../../../assets/portfolio/appoctober.jpg');
+const btm = require('../../../assets/portfolio/btm-performance.jpg');
+const coi = require('../../../assets/portfolio/coi.jpg');
+const decathlon = require('../../../assets/portfolio/decathlon.jpg');
+const emkt = require('../../../assets/portfolio/emkt.jpg');
+const portalUmesp = require('../../../assets/portfolio/portal-umesp.jpg');
+const totvs = require('../../../assets/portfolio/totvs.jpg');
+const t3Driver = require('../../../assets/portfolio/t3-driver-horizontal.jpg');
 
 module.exports = {
   aircnc,
+  appoctober,
+  btm,
+  coi,
+  decathlon,
+  emkt,
+  portalUmesp,
+  totvs,
+  t3Driver,
 };
