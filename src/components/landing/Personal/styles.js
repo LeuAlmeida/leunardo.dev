4baffb73969f34d1428787cf40ed7cd2508259bb
LeuAlmeida <leo@webid.net.br>
2020-02-11 19:03:55 -0300

    style: incrementing tastes and quirks

--- src/components/landing/Personal/styles.js
+++ src/components/landing/Personal/styles.js
@@ -28,7 +28,8 @@ export const Details = styled.div`
     width: 100%;
   }
 
-  h1 {
+  h1,
+  h3 {
     margin-bottom: 2rem;
     font-size: 26pt;
     color: #212121;
@@ -43,15 +44,26 @@ export const Details = styled.div`
   }
 `;
 
-export const Thumbnail = styled.div`
+export const TastesQuirksRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 40px;
+`;
+
+export const TasteOrQuirk = styled.div`
+  display: flex;
   flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  @media (max-width: 960px) {
-    width: 100%;
-    margin-bottom: 2rem;
-  }
-
-  img {
-    width: 100%;
+  span {
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #7178fc;
+    font-size: 22px;
+    margin-top: 8px;
   }
 `;
