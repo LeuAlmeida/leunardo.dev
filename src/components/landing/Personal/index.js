1e71e3084b11231373226bb369211d7bafb4a665
LeuAlmeida <leo@webid.net.br>
2020-02-12 17:27:45 -0300

    refactor: update Personal section after separe between components

--- src/components/landing/Personal/index.js
+++ src/components/landing/Personal/index.js
@@ -1,58 +1,16 @@
 import React from 'react';
 import { Container } from 'components/common';
 import CardInterests from 'components/common/CardInterests';
-import { FaCode, FaCoffee, FaMusic, FaTv, FaDumbbell, FaPencilRuler, FaBook, FaGamepad } from 'react-icons/fa';
-import { Wrapper, SkillsWrapper, Details, TastesQuirksRow, TasteOrQuirk, InterestSection } from './styles';
+import { Wrapper, SkillsWrapper, InterestSection, Details } from './styles';
 import { TitleWrapper, H2, SubTitle } from '../../common/Text';
+
+import About from './About';
+import LanguageCounter from '../../common/LanguageCounter';
 
 export const Personal = () => (
   <Wrapper id="myself">
     <SkillsWrapper as={Container}>
-      <TitleWrapper>
-        <H2>About Me</H2>
-        <SubTitle>Personal</SubTitle>
-      </TitleWrapper>
-      <Details>
-        <h3>Tastes and Quirks</h3>
-
-        <TastesQuirksRow>
-          <TasteOrQuirk>
-            <FaCode color="#333" size={64} />
-            <span>Code</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaCoffee color="#333" size={64} />
-            <span>Coffee</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaMusic color="#333" size={64} />
-            <span>Music</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaTv color="#333" size={64} />
-            <span>Series</span>
-          </TasteOrQuirk>
-        </TastesQuirksRow>
-
-        <TastesQuirksRow>
-          <TasteOrQuirk>
-            <FaDumbbell color="#333" size={64} />
-            <span>Workout</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaPencilRuler color="#333" size={64} />
-            <span>Studying</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaBook color="#333" size={64} />
-            <span>Books</span>
-          </TasteOrQuirk>
-          <TasteOrQuirk>
-            <FaGamepad color="#333" size={64} />
-            <span>Games</span>
-          </TasteOrQuirk>
-        </TastesQuirksRow>
-      </Details>
+      <About />
     </SkillsWrapper>
 
     <SkillsWrapper as={Container}>
@@ -79,6 +37,20 @@ export const Personal = () => (
             alert('Germany!');
           }}
         />
+      </InterestSection>
+    </SkillsWrapper>
+    <SkillsWrapper as={Container}>
+      <TitleWrapper>
+        <H2>About Me</H2>
+        <SubTitle>Skills</SubTitle>
+      </TitleWrapper>
+
+      <InterestSection>
+        <Details>
+          <h3>Languages</h3>
+        </Details>
+
+        <LanguageCounter />
       </InterestSection>
     </SkillsWrapper>
   </Wrapper>
