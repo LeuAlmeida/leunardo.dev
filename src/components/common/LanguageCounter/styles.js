0cee6ae70bc502a16c21952e76345603abc69cf4
LeuAlmeida <leo@webid.net.br>
2020-02-12 17:26:59 -0300

    style: create language counter stylesheet

---
+++ src/components/common/LanguageCounter/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 30px;
+  background: #222;
+  border-radius: 5px;
+  min-width: 300px;
+  width: 100%;
+  flex: 1;
+  padding: 20px;
+`;
+
+export const CountPs = styled.span`
+  margin-left: 5px;
+  color: #585858 !important;
+`;
