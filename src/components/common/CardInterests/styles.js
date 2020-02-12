e3a0c6184475439640af3f3d2a8a94381b3547bc
LeuAlmeida <leo@webid.net.br>
2020-02-12 17:26:23 -0300

    style: update card margin

--- src/components/common/CardInterests/styles.js
+++ src/components/common/CardInterests/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const CardContainer = styled.div`
   width: 100%;
   height: 100%;
+  margin-top: 30px;
   display: flex;
   justify-content: center;
   align-items: flex-start;
