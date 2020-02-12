96e7256418a46d71b7d19ee03dab13a43fc784c2
LeuAlmeida <leo@webid.net.br>
2020-02-12 13:30:46 -0300

    feat: introducing PropTypes

--- src/components/theme/Header/NavbarLinks/index.js
+++ src/components/theme/Header/NavbarLinks/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
 const NavbarLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
-    <AnchorLink href="#about">About</AnchorLink>
+    {/* <AnchorLink href="#about">About</AnchorLink> */}
     <AnchorLink href="#pinned">Repositories</AnchorLink>
     <AnchorLink href="#portfolio">Portfolio</AnchorLink>
+    <AnchorLink href="#myself">Who I am</AnchorLink>
     <AnchorLink href="#contact">Contact</AnchorLink>
   </Wrapper>
 );
+
+NavbarLinks.propTypes = {
+  desktop: PropTypes.bool,
+};
 
 export default NavbarLinks;
