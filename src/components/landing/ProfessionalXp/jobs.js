838378ddeede87c59c3a987717db19308600401d
LeuAlmeida <leo@webid.net.br>
2020-02-13 13:59:37 -0300

    feat: improving Professional Experience data content

--- src/components/landing/ProfessionalXp/jobs.js
+++ src/components/landing/ProfessionalXp/jobs.js
@@ -1,10 +1,33 @@
 export const jobs = [
   {
-    title: 'comment #01',
-    description: 'the best animation , the best toturials you would ever see .',
-    name: '- dr. mohamed -',
-    start: 'JUN, 17',
-    sup: 'th',
-    end: '12:00 AM',
+    title: 'Assistant Front End Developer',
+    description:
+      'Development of Web Projects with HTML, CSS3, Javascript and Plone language use, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+    company: 'Universidade Metodista de São Paulo',
+    start: 'JUN, 2019',
+    end: 'PRESENT',
+  },
+  {
+    title: 'Web Designer Intern',
+    description:
+      'Front-End content Update, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+    company: 'Universidade Metodista de São Paulo',
+    start: 'JUL, 2017',
+    end: 'JUN, 2019',
+  },
+  {
+    title: 'Web Designer Apprentice',
+    description: 'Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+    company: 'Universidade Metodista de São Paulo',
+    start: 'JUL, 2016',
+    end: 'JUN, 2017',
+  },
+  {
+    title: 'Administrative Assistant Apprentice',
+    description:
+      'Use of internal CSM, Administrative assistant on the subject of satellite tracking management, Client payment control.',
+    company: 'Insat Rastreadores',
+    start: 'APR, 2014',
+    end: 'APR, 2015',
   },
 ];
