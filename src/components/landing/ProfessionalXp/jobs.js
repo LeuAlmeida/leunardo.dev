9cc091d17a80de2cc0e9e211aa6487f1f21121cc
LeuAlmeida <leo@webid.net.br>
2020-02-13 13:13:49 -0300

    feat: create jobs data file

---
+++ src/components/landing/ProfessionalXp/jobs.js
@@ -0,0 +1,10 @@
+export const jobs = [
+  {
+    title: 'comment #01',
+    description: 'the best animation , the best toturials you would ever see .',
+    name: '- dr. mohamed -',
+    start: 'JUN, 17',
+    sup: 'th',
+    end: '12:00 AM',
+  },
+];
