5cbc30150cfaebc7eddcf542c4b4891ab1c1a178
LeuAlmeida <leo@webid.net.br>
2020-02-13 11:20:44 -0300

    feat: update PortfolioXp component

--- src/components/landing/ProfessionalXp/index.js
+++ src/components/landing/ProfessionalXp/index.js
@@ -1,170 +1,42 @@
 import React from 'react';
 
-import { Timeline } from './styles';
+import { Container } from './styles';
 
-export default function ProfessionalXp() {
+function Timeline({ title, description, name, start, sup, end }) {
+  return (
+    <div className="box">
+      <ul id="first-list">
+        <li>
+          <span></span>
+          <div className="title">{title}</div>
+          <div className="info">{description}</div>
+          <div className="name">{name}</div>
+          <div className="time">
+            <span>
+              {start}
+              <sup>{sup}</sup>
+            </span>
+            <span>{end}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export function ProfessionalXp() {
   return (
     <>
-      <Timeline>
-        <ul>
-          <li>
-            <div>
-              <time>1687</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Laws of motion</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Newton</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1785</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Law of electrostatic attraction</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Coulomb</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1827</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Law of Electric Resistance</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>G.S Ohm</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1831</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Electromagnetic Induction</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Michael Faraday</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1867</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Dynamite</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Alfred Nobel</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1895</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>X Rays</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Roentgen</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1897</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Electron</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>J.J.Thomson</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1898</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Radium</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Madam Curie</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1900</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Quantum Theory</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Max Plank</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1905</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Principle of Relativity</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Albert Einstein</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1905</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Photo electric effect</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Albert Einstein</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <time>1942</time>
-              <div className="discovery">
-                <h1>Discovery</h1>
-                <p>Nuclear Reactor</p>
-              </div>
-              <div className="scientist">
-                <h1>Scientist</h1>
-                <span>Anrico Fermi</span>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </Timeline>
+      <Container>
+        <Timeline
+          title="comment #01"
+          description="the best animation , the best toturials you would ever see ."
+          name="- dr. mohamed -"
+          start="JUN, 17"
+          sup="th"
+          end="12:00 AM"
+        />
+      </Container>
     </>
   );
 }
