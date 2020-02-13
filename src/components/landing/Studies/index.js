0ca3be7027de08a723e46cb3e0e9d05483cad7b2
LeuAlmeida <leo@webid.net.br>
2020-02-13 19:26:11 -0300

    feat: skill card improvements

--- src/components/landing/Studies/index.js
+++ src/components/landing/Studies/index.js
@@ -1,39 +1,47 @@
 import React from 'react';
-import { TitleWrapper, H2, SubTitle, Details } from '../../../common/Text';
-import { Container, SkillCard } from './styles';
+import { TitleWrapper, H2, SubTitle, Details } from '../../common/Text';
+import { Container, SkillCard, CardsSection } from './styles';
+import Tag from '../Personal/Techs/Tag';
 
-function Studies() {
+function CardStudy({ color, image, name, duration, description, certificate }) {
+  return (
+    <SkillCard>
+      <header className={`skill-card__header ${color}`}>
+        <img className="skill-card__icon" src={image} alt={name} />
+      </header>
+
+      <section className="skill-card__body">
+        <h2 className="skill-card__title">{name}</h2>
+        <span className="skill-card__duration">{duration}</span>
+        <ul className="skill-card__knowledge">
+          <li>{description}</li>
+          {certificate ? <Tag color="blue" url={certificate} name="Certificate" icon="certificate" /> : ''}
+        </ul>
+      </section>
+    </SkillCard>
+  );
+}
+
+export function Studies() {
   return (
     <Container>
       <TitleWrapper>
-        <H2>#SendJobs</H2>
-        <SubTitle>Past Jobs</SubTitle>
+        <H2>Hello World</H2>
+        <SubTitle>Studies</SubTitle>
       </TitleWrapper>
       <Details>
-        <h3>Professional Experience</h3>
-
-        <SkillCard>
-          <header className="skill-card__header">
-            <img
-              className="skill-card__icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
-              alt="HTML5 Logo"
-            />
-          </header>
-          <section className="skill-card__body">
-            <h2 className="skill-card__title">html</h2>
-            <span className="skill-card__duration">6 ans d'expérience</span>
-            <ul className="skill-card__knowledge">
-              <li>HTML5</li>
-              <li>Sémantique</li>
-              <li>SVG</li>
-              <li>Canvas</li>
-            </ul>
-          </section>
-        </SkillCard>
+        <h3>Certificates and Courses</h3>
+        <CardsSection>
+          <CardStudy
+            image="https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
+            duration="6 ans d'expérience"
+            color="orange"
+            name="HTML5"
+            description="ok"
+            certificate="https://google.com.br"
+          />
+        </CardsSection>
       </Details>
     </Container>
   );
 }
-
-export default Studies;
