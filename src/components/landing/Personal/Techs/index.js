21a2c8964e28796771dacd2c99529abdf71c3209
LeuAlmeida <leo@webid.net.br>
2020-02-13 13:04:25 -0300

    refactor: update Details styled component location

--- src/components/landing/Personal/Techs/index.js
+++ src/components/landing/Personal/Techs/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FaCode, FaPencilRuler } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import { Details, Container, Badges } from './styles';
-import { TitleWrapper, H2, SubTitle } from '../../../common/Text';
+import { Container, Badges } from './styles';
+import { TitleWrapper, H2, SubTitle, Details } from '../../../common/Text';
 
 function Tech({ name, color, icon }) {
   return (
@@ -30,8 +30,8 @@ function Techs() {
     <>
       <Container style={{ paddingTop: 40 }}>
         <TitleWrapper>
-          <H2>About Me</H2>
-          <SubTitle>Personal</SubTitle>
+          <H2>#Techs</H2>
+          <SubTitle>About Me</SubTitle>
         </TitleWrapper>
         <Details>
           <h3>Technical Skills</h3>
