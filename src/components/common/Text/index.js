122a8b5cfc2090c8e8cdd08ab22d29f81795c25e
LeuAlmeida <leo@webid.net.br>
2020-02-13 13:00:53 -0300

    feat: introducing Details into common text styled component

--- src/components/common/Text/index.js
+++ src/components/common/Text/index.js
@@ -29,3 +29,65 @@ export const SubTitle = styled.span`
   font-weight: bold;
   color: #666;
 `;
+
+export const Details = styled.div`
+  flex: 1;
+  padding-left: 2rem;
+
+  h3 {
+    margin-bottom: 2rem;
+    font-size: 26pt;
+    color: #212121;
+  }
+
+  h4 {
+    font-size: 26pt;
+    color: #8c8c8c;
+    font-weight: 400;
+  }
+
+  @media (max-width: 960px) {
+    padding-left: unset;
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 960px) {
+    padding-top: 4rem;
+
+    h1,
+    h4 {
+      margin-left: 10%;
+    }
+
+    h4 {
+      font-size: 26pt;
+    }
+  }
+
+  @media only screen and (max-width: 568px) {
+    padding-top: 4rem;
+
+    h1 {
+      margin-left: 10%;
+      font-size: 28pt;
+    }
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+    font-size: 40pt;
+    color: #212121;
+  }
+  h3 {
+    margin-bottom: 2rem;
+    color: #212121;
+  }
+
+  p {
+    margin-bottom: 2.5rem;
+    font-size: 14pt;
+    font-weight: normal;
+    line-height: 1.3;
+    color: #707070;
+  }
+`;
