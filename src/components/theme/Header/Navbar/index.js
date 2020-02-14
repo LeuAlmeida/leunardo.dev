1bd4dadd2f7a9c58504f1f92780584e88bbe24b7
LeuAlmeida <leo@webid.net.br>
2020-02-14 19:20:11 -0300

    feat(Navbar): improving navbar logo

--- src/components/theme/Header/Navbar/index.js
+++ src/components/theme/Header/Navbar/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { Container } from 'components/common';
 import NavbarLinks from '../NavbarLinks';
-import { Wrapper } from './styles';
+import { Wrapper, Logo } from './styles';
 
 const Navbar = () => (
   <Wrapper as={Container}>
-    <Link to="/">Léu Almeida</Link>
+    <Logo />
     <NavbarLinks desktop />
   </Wrapper>
 );
