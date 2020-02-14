a0a4022f904c2476895fcaad023fc7c236aa0c80
LeuAlmeida <leo@webid.net.br>
2020-02-14 19:18:20 -0300

    feat(Portfolio): container improvements

--- src/components/landing/Portfolio/index.js
+++ src/components/landing/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { HeaderContainer, FilterWrapper, FilterItem, Container, Grid, PortfolioWrapper } from './styles';
+import { HeaderContainer, FilterWrapper, FilterItem, Container, Grid, PortfolioWrapper, SkillsWrapper } from './styles';
 import ImagePortfolio from '../../common/ImagePortfolio';
 import { TitleWrapper, H2, SubTitle } from '../../common/Text';
 import { portfolio } from './portfolio';
@@ -23,32 +23,34 @@ export const Portfolio = () => {
   }, [filter]);
   return (
     <>
-      <HeaderContainer id="portfolio">
-        <TitleWrapper style={{ marginLeft: '10%' }}>
-          <H2>Portfólio</H2>
-          <SubTitle>Recent Jobs</SubTitle>
-        </TitleWrapper>
-        <FilterWrapper>
-          <FilterItem active={filter === 'all'} onClick={() => setFilter('all')}>
-            All
-          </FilterItem>
-          <FilterItem active={filter === 'frontend'} onClick={() => setFilter('frontend')}>
-            Frontend
-          </FilterItem>
-          <FilterItem active={filter === 'mobile'} onClick={() => setFilter('mobile')}>
-            Mobile
-          </FilterItem>
-          <FilterItem active={filter === 'backend'} onClick={() => setFilter('backend')}>
-            Backend
-          </FilterItem>
-          <FilterItem active={filter === 'ux-ui'} onClick={() => setFilter('ux-ui')}>
-            UX/UI
-          </FilterItem>
-          <FilterItem active={filter === 'others'} onClick={() => setFilter('others')}>
-            Others
-          </FilterItem>
-        </FilterWrapper>
-      </HeaderContainer>
+      <SkillsWrapper as={Container} id="portfolio">
+        <HeaderContainer>
+          <TitleWrapper style={{ marginLeft: '10%' }}>
+            <H2>Portfólio</H2>
+            <SubTitle>Recent Jobs</SubTitle>
+          </TitleWrapper>
+          <FilterWrapper>
+            <FilterItem active={filter === 'all'} onClick={() => setFilter('all')}>
+              All
+            </FilterItem>
+            <FilterItem active={filter === 'frontend'} onClick={() => setFilter('frontend')}>
+              Frontend
+            </FilterItem>
+            <FilterItem active={filter === 'mobile'} onClick={() => setFilter('mobile')}>
+              Mobile
+            </FilterItem>
+            <FilterItem active={filter === 'backend'} onClick={() => setFilter('backend')}>
+              Backend
+            </FilterItem>
+            <FilterItem active={filter === 'ux-ui'} onClick={() => setFilter('ux-ui')}>
+              UX/UI
+            </FilterItem>
+            <FilterItem active={filter === 'others'} onClick={() => setFilter('others')}>
+              Others
+            </FilterItem>
+          </FilterWrapper>
+        </HeaderContainer>
+      </SkillsWrapper>
       <PortfolioWrapper>
         <Container>
           <Grid>
