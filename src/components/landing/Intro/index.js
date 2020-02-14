ff2acc27148c5894872180a3df21ab7972fd1604
LeuAlmeida <leo@webid.net.br>
2020-02-14 16:51:35 -0300

    feat(Intro): introducing social media tooltips

--- src/components/landing/Intro/index.js
+++ src/components/landing/Intro/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
+import ReactTooltip from 'react-tooltip';
+import { FaGithub, FaBehance, FaLinkedin, FaInstagram, FaFacebookF, FaFilePdf } from 'react-icons/fa';
+import useTypewriter from 'react-typewriter-hook';
+
 import { Header } from 'components/theme';
 import { Container, ButtonIcon } from 'components/common';
 import { Details } from 'components/common/Text';
+
 import dev from 'assets/illustrations/dev.svg';
-import { FaGithub, FaBehance, FaLinkedin, FaInstagram, FaFacebookF, FaFilePdf } from 'react-icons/fa';
-import useTypewriter from 'react-typewriter-hook';
 import { Wrapper, IntroWrapper, Thumbnail, SocialIcons } from './styles';
 
 const MagicOcean = ['Backend Developer', 'UX & UI Designer'];
@@ -25,38 +28,53 @@ export function Intro() {
   }, [magicName]);
 
   return (
-    <Wrapper id="about">
-      <Header />
-      <IntroWrapper as={Container}>
-        <Details>
-          <h1>Hello, I'm Léu Almeida</h1>
-          <h4>I am {name}</h4>
-          <h6>Stalk me in</h6>
-          <SocialIcons>
-            <ButtonIcon href="https://github.com/LeuAlmeida">
-              <FaGithub color="#FFF" size={18} />
-            </ButtonIcon>
-            <ButtonIcon href="https://www.behance.net/almeida99">
-              <FaBehance color="#FFF" size={18} />
-            </ButtonIcon>
-            <ButtonIcon href="https://www.linkedin.com/in/leonardoalmeida99/">
-              <FaLinkedin color="#FFF" size={18} />
-            </ButtonIcon>
-            <ButtonIcon href="https://www.instagram.com/1bigo/">
-              <FaInstagram color="#FFF" size={18} />
-            </ButtonIcon>
-            <ButtonIcon href="https://www.facebook.com/leuzinhopataty">
-              <FaFacebookF color="#FFF" size={18} />
-            </ButtonIcon>
-            <ButtonIcon href="https://leunardo.dev/cv">
-              <FaFilePdf color="#FFF" size={18} />
-            </ButtonIcon>
-          </SocialIcons>
-        </Details>
-        <Thumbnail>
-          <img src={dev} alt="Hello! My name is Leonardo Almeida, but you can call me Léu :)" />
-        </Thumbnail>
-      </IntroWrapper>
-    </Wrapper>
+    <>
+      <Wrapper id="about">
+        <Header />
+        <IntroWrapper as={Container}>
+          <Details>
+            <h1>Hello, I'm Léu Almeida</h1>
+            <h4>I am {name}</h4>
+            <h6>Stalk me in</h6>
+            <SocialIcons>
+              <ButtonIcon href="https://github.com/LeuAlmeida" data-tip data-for="github">
+                <FaGithub color="#FFF" size={18} />
+              </ButtonIcon>
+              <ButtonIcon href="https://www.behance.net/almeida99" data-tip data-for="behance">
+                <FaBehance color="#FFF" size={18} />
+              </ButtonIcon>
+              <ButtonIcon href="https://www.linkedin.com/in/leonardoalmeida99/" data-tip data-for="linkedin">
+                <FaLinkedin color="#FFF" size={18} />
+              </ButtonIcon>
+              <ButtonIcon href="https://www.instagram.com/1bigo/" data-tip data-for="instagram">
+                <FaInstagram color="#FFF" size={18} />
+              </ButtonIcon>
+              <ButtonIcon href="https://www.facebook.com/leuzinhopataty" data-tip data-for="facebook">
+                <FaFacebookF color="#FFF" size={18} />
+              </ButtonIcon>
+              <ButtonIcon href="https://leunardo.dev/cv" data-tip data-for="cv">
+                <FaFilePdf color="#FFF" size={18} />
+              </ButtonIcon>
+            </SocialIcons>
+          </Details>
+          <Thumbnail>
+            <img src={dev} alt="Hello! My name is Leonardo Almeida, but you can call me Léu :)" />
+          </Thumbnail>
+        </IntroWrapper>
+      </Wrapper>
+
+      <ReactTooltip id="linkedin" type="dark" effect="solid">
+        <span>LinkedIn</span>
+      </ReactTooltip>
+      <ReactTooltip id="instagram" type="dark" effect="solid">
+        <span>Instagram</span>
+      </ReactTooltip>
+      <ReactTooltip id="facebook" type="dark" effect="solid">
+        <span>Facebook</span>
+      </ReactTooltip>
+      <ReactTooltip id="cv" type="dark" effect="solid">
+        <span>Curriculum</span>
+      </ReactTooltip>
+    </>
   );
 }
