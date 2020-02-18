00a390f80f4865d60e5a28296251b0c996757135
LeuAlmeida <leo@webid.net.br>
2020-02-18 12:25:56 -0300

    feat(pages): implementing Redux on Pages index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Layout, SEO } from 'components/common';
 import {
   Intro,
@@ -12,19 +13,24 @@ import {
   Blockquote,
 } from 'components/landing';
 import FloatMenu from 'components/floatMenu';
+import Footer from '../components/theme/Footer';
+import store from '../store';
 
 export default () => (
-  <Layout>
-    <SEO />
-    <FloatMenu />
-    <Intro />
-    <Projects />
-    <Portfolio />
-    <Skills />
-    <Personal />
-    <ProfessionalXp />
-    <Studies />
-    <Blockquote />
-    <Contact />
-  </Layout>
+  <Provider store={store}>
+    <Layout>
+      <SEO />
+      <FloatMenu />
+      <Intro />
+      <Projects />
+      <Portfolio />
+      <Skills />
+      <Personal />
+      <ProfessionalXp />
+      <Studies />
+      <Blockquote />
+      <Contact />
+      <Footer />
+    </Layout>
+  </Provider>
 );
