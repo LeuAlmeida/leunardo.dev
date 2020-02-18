7a90f4225c52841b423e6c826c88b3224c62216a
LeuAlmeida <leo@webid.net.br>
2020-02-18 18:05:45 -0300

    feat(professionalxp/jobs): create portuguese translation of Jobs

--- src/components/landing/ProfessionalXp/jobs.js
+++ src/components/landing/ProfessionalXp/jobs.js
@@ -1,33 +1,68 @@
-export const jobs = [
-  {
-    title: 'Assistant Front End Developer',
-    description:
-      'Development of Web Projects with HTML, CSS3, Javascript and Plone language use, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
-    company: 'Universidade Metodista de São Paulo',
-    start: 'JUN, 2019',
-    end: 'PRESENT',
-  },
-  {
-    title: 'Web Designer Intern',
-    description:
-      'Front-End content Update, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
-    company: 'Universidade Metodista de São Paulo',
-    start: 'JUL, 2017',
-    end: 'JUN, 2019',
-  },
-  {
-    title: 'Web Designer Apprentice',
-    description: 'Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
-    company: 'Universidade Metodista de São Paulo',
-    start: 'JUL, 2016',
-    end: 'JUN, 2017',
-  },
-  {
-    title: 'Administrative Assistant Apprentice',
-    description:
-      'Use of internal CSM, Administrative assistant on the subject of satellite tracking management, Client payment control.',
-    company: 'Insat Rastreadores',
-    start: 'APR, 2014',
-    end: 'APR, 2015',
-  },
-];
+export const jobs = {
+  english: [
+    {
+      title: 'Assistant Front End Developer',
+      description:
+        'Development of Web Projects with HTML, CSS3, Javascript and Plone language use, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUN, 2019',
+      end: 'PRESENT',
+    },
+    {
+      title: 'Web Designer Intern',
+      description:
+        'Front-End content Update, Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUL, 2017',
+      end: 'JUN, 2019',
+    },
+    {
+      title: 'Web Designer Apprentice',
+      description: 'Production and dispatching of marketing mail, Production and dispatching of SMS marketing.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUL, 2016',
+      end: 'JUN, 2017',
+    },
+    {
+      title: 'Administrative Assistant Apprentice',
+      description:
+        'Use of internal CSM, Administrative assistant on the subject of satellite tracking management, Client payment control.',
+      company: 'Insat Rastreadores',
+      start: 'APR, 2014',
+      end: 'APR, 2015',
+    },
+  ],
+  portuguese: [
+    {
+      title: 'Assistente Desenvolvedor Front-End',
+      description:
+        'Desenvolvimento de projetos web com o uso de linguagens como HTML5, CSS3, JavaScript e Plone; Criação e disparo de e-mail marketing; Criação e envio de SMS marketing.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUN, 2019',
+      end: 'ATUAL',
+    },
+    {
+      title: 'Estagiário Web Designer',
+      description:
+        'Criação e disparo de e-mail marketing; Criação e envio de SMS marketing; Atualização de conteúdo front-end.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUL, 2017',
+      end: 'JUN, 2019',
+    },
+    {
+      title: 'Aprendiz Web Designer',
+      description: 'Criação e disparo de e-mail marketing; Criação e envio de SMS marketing.',
+      company: 'Universidade Metodista de São Paulo',
+      start: 'JUL, 2016',
+      end: 'JUN, 2017',
+    },
+    {
+      title: 'Aprendiz Auxiliar Administrativo',
+      description:
+        'Uso de CMS interno; Auxiliar administrativo quanto o gerenciamento de rastreadores via satélite; Controle de clientes adimplentes e inadimplentes.',
+      company: 'Insat Rastreadores',
+      start: 'ABR, 2014',
+      end: 'ABR, 2015',
+    },
+  ],
+};
