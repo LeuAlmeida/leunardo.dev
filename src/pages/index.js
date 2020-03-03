fe04612574de59d08476ff1fc50e188c799e75ef
LeuAlmeida <leo@webid.net.br>
2020-03-03 19:00:01 -0300

    feat(pages): introducing Toast into pages file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import { Layout, SEO } from 'components/common';
 import { Contact } from 'components/landing';
@@ -14,9 +15,12 @@ import Blockquote from '../components/landing/Blockquote';
 import Footer from '../components/theme/Footer';
 import store from '../store';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 export default () => (
   <Provider store={store}>
     <Layout>
+      <ToastContainer autoClose={3000} position="top-left" />
       <SEO />
       <FloatMenu />
       <Intro />
