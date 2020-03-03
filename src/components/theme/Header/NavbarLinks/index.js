11fb18835828e9a9e77cb7ac8ebffa495a33e673
LeuAlmeida <leo@webid.net.br>
2020-03-03 18:59:14 -0300

    refactor(NavbarLinks): incrementing translate into navbar

--- src/components/theme/Header/NavbarLinks/index.js
+++ src/components/theme/Header/NavbarLinks/index.js
@@ -1,12 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
 function NavbarLinks({ desktop, language }) {
+  const dispatch = useDispatch();
+
   const [lang, setLang] = useState('english');
+
+  useEffect(() => {
+    let currentLang;
+
+    const userlang = navigator.language || navigator.userLanguage;
+    if (userlang === 'pt-BR' || userlang === 'pt') {
+      currentLang = 'portuguese';
+    } else if (userlang === 'de') {
+      currentLang = 'germany';
+    } else {
+      currentLang = 'english';
+    }
+
+    dispatch({
+      type: 'SET_LANGUAGE',
+      language: currentLang,
+    });
+  }, [dispatch]);
+
+  function setAsLanguage(language) {
+    if (language === 'portuguese') {
+      dispatch({
+        type: 'SET_LANGUAGE',
+        language,
+      });
+
+      if (lang === 'portuguese') {
+        toast.info(`Você já está aqui. 😜`);
+        return null;
+      }
+
+      toast.success(`Bem-vindo ao meu site!`);
+      setLang('portuguese');
+    }
+
+    if (language === 'english') {
+      dispatch({
+        type: 'SET_LANGUAGE',
+        language,
+      });
+
+      if (lang === 'english') {
+        toast.info(`You're already here. 😜`);
+        return null;
+      }
+
+      toast.success(`Welcome to my site!`);
+      setLang('english');
+    }
+
+    if (language === 'germany') {
+      if (lang === 'english') {
+        toast.error(`German translation doesn't available yet. 😓`);
+        return null;
+      }
+      if (lang === 'portuguese') {
+        toast.error(`O idioma alemão ainda não está disponível. 😓`);
+        return null;
+      }
+    }
+  }
 
   useEffect(() => {
     setLang(language || lang);
@@ -21,6 +85,15 @@ function NavbarLinks({ desktop, language }) {
           <AnchorLink href="#myself">Who I am</AnchorLink>
           <AnchorLink href="#experience">Experience</AnchorLink>
           <AnchorLink href="#contact">Contact</AnchorLink>
+          <button onClick={() => setAsLanguage('portuguese')} type="button">
+            🇧🇷
+          </button>
+          <button onClick={() => setAsLanguage('english')} type="button">
+            🇬🇧
+          </button>
+          <button onClick={() => setAsLanguage('german')} type="button">
+            🇩🇪
+          </button>
         </>
       )}
       {lang === 'portuguese' && (
@@ -30,6 +103,15 @@ function NavbarLinks({ desktop, language }) {
           <AnchorLink href="#myself">Quem sou eu</AnchorLink>
           <AnchorLink href="#experience">Experiência</AnchorLink>
           <AnchorLink href="#contact">Contato</AnchorLink>
+          <button onClick={() => setAsLanguage('portuguese')} type="button">
+            🇧🇷
+          </button>
+          <button onClick={() => setAsLanguage('english')} type="button">
+            🇬🇧
+          </button>
+          <button onClick={() => setAsLanguage('german')} type="button">
+            🇩🇪
+          </button>
         </>
       )}
     </Wrapper>
