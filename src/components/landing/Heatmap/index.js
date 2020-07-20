8195e1678193d2b0a8fde3062feb5d438e4fe2d6
Léu Almeida <leo@webid.net.br>
2020-07-20 16:28:02 -0300

    feat: implementing github heatmap

--- src/components/landing/Heatmap/index.js
+++ src/components/landing/Heatmap/index.js
@@ -2,60 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { Container, Card } from 'components/common';
-import starIcon from 'assets/icons/star.svg';
-import forkIcon from 'assets/icons/fork.svg';
-import { Wrapper, Grid, Item, Content, Stats } from './styles';
-
-import api from '../../../services/api';
+import { Container } from 'components/common';
+import { Wrapper, Grid } from './styles';
 
 const Heatmap = ({ language }) => {
-  const [edges, setEdges] = useState([]);
-
   const [lang, setLang] = useState('english');
 
   useEffect(() => {
     setLang(language || lang);
   }, [lang, language]);
 
-  useEffect(() => {
-    async function loadGithub() {
-      const response = await api.get('/users/LeuAlmeida/repos?per_page=150');
-
-      const data = response.data.sort((a, b) => (a.stargazers_count > b.stargazers_count ? -1 : 1)).slice(0, 6);
-
-      setEdges(data);
-    }
-
-    loadGithub();
-  }, []);
-
   return (
     <Wrapper as={Container} id="pinned">
-      {lang === 'english' && <h2>Top Starred Repositories</h2>}
-      {lang === 'portuguese' && <h2>Repositórios Mais Favoritados</h2>}
-      {lang === 'german' && <h2>Top Markierte Repositories</h2>}
+      {lang === 'english' && <h2>Github Code Frequency</h2>}
+      {lang === 'portuguese' && <h2>Frequência no Github</h2>}
+      {lang === 'german' && <h2>Github-Code-Frequenz</h2>}
       <Grid>
-        {edges.map(node => (
-          <Item key={node.id} as="a" href={node.html_url} target="_blank" rel="noopener noreferrer">
-            <Card>
-              <Content>
-                <h4>{node.name}</h4>
-                <p>{node.description}</p>
-              </Content>
-              <Stats>
-                <div>
-                  <img src={starIcon} alt="stars" />
-                  <span>{node.stargazers_count}</span>
-                </div>
-                <div>
-                  <img src={forkIcon} alt="forks" />
-                  <span>{node.forks_count}</span>
-                </div>
-              </Stats>
-            </Card>
-          </Item>
-        ))}
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/LeuAlmeida">
+          <img
+            alt="Github Frequency"
+            src="https://grass-graph.moshimo.works/images/LeuAlmeida.png?width=870&height=155&background=none"
+          />
+        </a>
       </Grid>
     </Wrapper>
   );
--- src/components/landing/Heatmap/styles.js
+++ src/components/landing/Heatmap/styles.js
@@ -7,9 +7,11 @@ export const Wrapper = styled.div`
 export const Grid = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-template-rows: 8fr;
   gap: 1.2rem 1.2rem;
+
+  width: 100%;
 
   @media (max-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
